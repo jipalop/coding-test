524776bf5ce4c7f64b7060245c04fb9de107efed
Jose Ignacio Palop Fernandez <jose.palopfernandez@verramobility.com>
2024-12-09 13:14:05 +0100

    create product repository

---
+++ src/Modules/Product/Infrastructure/Persistence/InMemoryProductRepository.php
@@ -0,0 +1,62 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Modules\Product\Infrastructure\Persistence;
+
+use App\Modules\Product\Domain\Product;
+use App\Modules\Product\Domain\ProductNotExists;
+use App\Modules\Product\Domain\ProductRepository;
+use App\Modules\Shared\Domain\ProductId;
+
+class InMemoryProductRepository implements ProductRepository
+{
+    private const PRODUCTS = [
+        'A101' => [
+            'id' => 'A101',
+            'description' => 'Screwdriver',
+            'category' => 1,
+            'price' => 9.75
+        ],
+        'A102' => [
+            'id' => 'A102',
+            'description' => 'Electric screwdriver',
+            'category' => 1,
+            'price' => 49.50
+        ],
+        'B101' => [
+            'id' => 'B101',
+            'description' => 'Basic on-off switch',
+            'category' => 2,
+            'price' => 4.99
+        ],
+        'B102' => [
+            'id' => 'B102',
+            'description' => 'Press button',
+            'category' => 2,
+            'price' => 4.99
+        ],
+        'B103' => [
+            'id' => 'B103',
+            'description' => 'Switch with motion detector',
+            'category' => 2,
+            'price' => 12.95
+        ],
+    ];
+
+    public function find(ProductId $productId): Product
+    {
+        if (!isset(self::PRODUCTS[$productId->value()])) {
+
+            throw ProductNotExists::withId($productId);
+        }
+        $productData = self::PRODUCTS[$productId->value()];
+        return Product::create(
+            $productId->value(),
+            $productData['description'],
+            $productData['category'],
+            $productData['price']
+        );
+    }
+
+}
---
+++ src/Modules/Product/Test/InMemoryProductRepositoryTest.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Modules\Product\Test;
+
+use App\Modules\Product\Domain\Product;
+use App\Modules\Product\Domain\ProductNotExists;
+use App\Modules\Product\Infrastructure\Persistence\InMemoryProductRepository;
+use App\Modules\Shared\Domain\ProductId;
+use PHPUnit\Framework\TestCase;
+
+class InMemoryProductRepositoryTest extends TestCase
+{
+    public static function findProductDataProvider()
+    {
+        return [
+            ['A101', Product::create('A101', 'Screwdriver', 1, 9.75)],
+            ['A102', Product::create('A102', 'Electric screwdriver', 1, 49.50)],
+            ['B101', Product::create('B101', 'Basic on-off switch', 2, 4.99)],
+            ['B102', Product::create('B102', 'Press button', 2, 4.99)],
+            ['B103', Product::create('B103', 'Switch with motion detector', 2, 12.95)],
+        ];
+    }
+
+    /**
+     * @dataProvider findProductDataProvider
+     */
+    public function testItShouldFindAndReturnCustomer(string $id, Product $expectedProduct): void
+    {
+        $repo = new InMemoryProductRepository();
+        $product = $repo->find(new ProductId($id));
+
+        $this->assertEquals($product->id(), $expectedProduct->id());
+        $this->assertEquals($product->description(), $expectedProduct->description());
+        $this->assertEquals($product->category(), $expectedProduct->category());
+        $this->assertEquals($product->price(), $expectedProduct->price());
+    }
+
+    public function testItShouldNotFindAndThrowException(): void
+    {
+        $this->expectException(ProductNotExists::class);
+        $repo = new InMemoryProductRepository();
+        $repo->find(new ProductId(0));
+    }
+}
