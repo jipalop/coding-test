1af1965b19cc4bab224d565190476e463b0f0306
Jose Ignacio Palop Fernandez <jose.palopfernandez@verramobility.com>
2024-12-09 13:15:44 +0100

    shared value objects

---
+++ src/Modules/Shared/Domain/CustomerId.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace App\Modules\Shared\Domain;
+
+use App\Types\ValueObject\IntegerValueObject;
+
+final class CustomerId extends IntegerValueObject
+{
+
+}
---
+++ src/Modules/Shared/Domain/ProductPrice.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Modules\Shared\Domain;
+
+use App\Types\ValueObject\FloatValueObject;
+
+final class ProductPrice extends FloatValueObject
+{
+
+}
